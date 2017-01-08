353957f375aa4b0fa0d80c7da62f2f7cb817fe8c
Leah W. Chang <leahwchang@gmail.com>
2017-01-07 23:31:21 -0500

    fixed parseInt function to add base-10

--- 01.04.17-checking-input/javascript.js
+++ 01.04.17-checking-input/javascript.js
@@ -1,9 +1,9 @@
-var userAge = parseInt(prompt());
-    if (userAge = isNaN()) {
+var userAge = parseInt(prompt("What is your age?"), 10);
+if (isNaN(userAge) == true) {
     console.log("that is not a number");
 }   else if (userAge < 18) {
     console.log("you are under 18");   
-}   else if (userAge = 26) {
+}   else if (userAge == 26) {
     console.log("cool, you are the same age as me");
 }   else {
     console.log("hello");
