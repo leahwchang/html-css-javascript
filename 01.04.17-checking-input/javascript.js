8d2a20162dd258cca3ae05b88013f41eae363363
Leah W. Chang <leahwchang@gmail.com>
2017-01-07 23:23:58 -0500

    function for checking input

---
+++ 01.04.17-checking-input/javascript.js
@@ -0,0 +1,10 @@
+var userAge = parseInt(prompt());
+    if (userAge = isNaN()) {
+    console.log("that is not a number");
+}   else if (userAge < 18) {
+    console.log("you are under 18");   
+}   else if (userAge = 26) {
+    console.log("cool, you are the same age as me");
+}   else {
+    console.log("hello");
+}
