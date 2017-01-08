8aa2b6fc8a3f55974b142b45bb68ae14eb7ad296
Leah W. Chang <leahwchang@gmail.com>
2017-01-07 23:31:53 -0500

    fixed isnan function

--- 01.04.17-checking-input/javascript.js
+++ 01.04.17-checking-input/javascript.js
@@ -1,8 +1,8 @@
 var userAge = parseInt(prompt("What is your age?"), 10);
-if (isNaN(userAge) == true) {
+if (isNaN(userAge)) {
     console.log("that is not a number");
 }   else if (userAge < 18) {
-    console.log("you are under 18");   
+    console.log("you are under 18");
 }   else if (userAge == 26) {
     console.log("cool, you are the same age as me");
 }   else {
